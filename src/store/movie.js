a4bd319983b1f338593a87ae6057e2bb72f0b777
sacultang <sacultang@gmail.com>
2022-04-19 12:44:25 +0900

    fix : movie.js http -> https

--- src/store/movie.js
+++ src/store/movie.js
@@ -106,8 +106,8 @@ function _fetchMovie(payload){
   const {title,type,page,year,id} = payload
   const OMDB_API_KEY = 'f6573a61'
   const url = id 
-    ? `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
   // const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}`
 
   return new Promise((resolve,reject)=>{
